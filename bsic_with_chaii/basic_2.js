e6b8975b86dfdedb74979f163baa02fbf8ce709a
Zurnab7 <155717759+Zurnab7@users.noreply.github.com>
2024-09-17 11:23:53 +0000

    indexing

--- bsic_with_chaii/basic_2.js
+++ bsic_with_chaii/basic_2.js
@@ -1,5 +1,9 @@
 //*********ARRRAY *************/
-const myArray =[0,1,2,3,4,5];
+const myArray =[0,1,2,3,4,5,9];
 //myArray.push(8); push add the new value at the end of arry
 //myArray.pop(); pop remove the last value of the arry
+//myArray.unshift(890); unshift add the value in the start of the array
+//myArray.shift(); it will remove the starting point as opposite to unshift
+//console.log(myArray.includes(9));describe us the elements is present inside the array or not
+//console.log(myArray.indexOf(9)); describe the indexing of element in the array
 console.log(myArray);
