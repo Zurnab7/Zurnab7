a173200416d94864fe1a2433e90cd288c6aef054
Zurnab7 <155717759+Zurnab7@users.noreply.github.com>
2024-09-17 11:07:50 +0000

    push

--- bsic_with_chaii/basic_2.js
+++ bsic_with_chaii/basic_2.js
@@ -1,2 +1,5 @@
 //*********ARRRAY *************/
 const myArray =[0,1,2,3,4,5];
+//myArray.push(8); push add the new value at the end of arry
+//myArray.pop(); pop remove the last value of the arry
+console.log(myArray);
