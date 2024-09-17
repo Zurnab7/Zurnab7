a870805594cc3c0ba6ff22f4c9e105c5a06de1c5
Zurnab7 <155717759+Zurnab7@users.noreply.github.com>
2024-09-17 10:59:10 +0000

    new

--- bsic_with_chaii/2_conversion_operation.js
+++ bsic_with_chaii/2_conversion_operation.js
@@ -128,4 +128,10 @@ const max = 20;
 console.log(Math.floor(Math.random() * (max - min +1 ))+ min) ;
 //*************Date************ */
 let myDate = new Date;
-console.log(myDate); 
+console.log(myDate.toString()); 
+console.log(myDate.toDateString());
+console.log(myDate.toISOString);
+console.log(myDate.toJSON);
+console.log(myDate.toLocaleDateString); 
+console.log(Date.now());
+
